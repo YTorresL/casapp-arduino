3154e4465b70d12e69b6c7c9ff8eefbf784204d7
Yalith Torres <yalithdev@gmail.com>
2024-10-25 23:21:00 -0400

    Servo, fix bug proccess freeze

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ import gc
 import time
 import urequests as requests
 import read as rfid
-import machine 
+import machine
 
 gc.collect()
 
@@ -61,6 +61,23 @@ STATE_STATUS = {
     "WARNING": 2
 }
 
+def servo_move(angulo, servo=machine.PWM(machine.Pin(15), freq=50)):
+    if angulo < 0:
+        angulo = 0
+    elif angulo > 180:
+        angulo = 180
+    duty = int(40 + (angulo / 180) * 115)  # Conversión a ciclo de trabajo
+    servo.duty(duty)
+
+# Lógica para mover el servo al verificar acceso
+def servo_control():
+    print("Moviendo servo a 90 grados")
+    servo_move(90)
+    time.sleep(7)
+    print("Regresando servo a 0 grados")
+    servo_move(0)
+    time.sleep(1)
+
 def connectWifi(ssid, password):
     station = network.WLAN(network.STA_IF)
     station.active(True)
@@ -101,63 +118,117 @@ def extract_info(data, key):
     
 def house_access_control_status(data, code):
     try:
+        users_in_house = 0  # Contador para usuarios dentro de la casa
         for house in data:
             for access in house['attributes']['house_access_controls']['data']:
                 attributes = access['attributes']
+
+                # Obtener los logs de entrada de la casa
+                house_entry_logs = attributes['house_entry_logs']['data']
+                
+                # Verificar si existen logs
+                if house_entry_logs:
+                    last_log = house_entry_logs[-1]
+                    # Verificar el estado del último log
+                    if last_log['attributes']['status'] == USER_STATUS['IN_HOUSE']:
+                        users_in_house += 1  # Incrementar contador de usuarios en casa
                 
                 # Verificar código y estado de activación
                 if attributes['code'] == code and attributes['status'] == ACCESS_STATUS['ACTIVATED']:
                     log_api_url = f"{BASE_API_URL}{URL_LOG}"
 
-                    # Verificar si existen logs y el último log
-                    house_entry_logs = attributes['house_entry_logs']['data']
-                    if house_entry_logs:
-                        last_log = house_entry_logs[-1]
+                    # Determinar el nuevo estado
+                    new_status = USER_STATUS['IN_HOUSE'] if not house_entry_logs else (
+                        USER_STATUS['OUT_HOUSE'] if last_log['attributes']['status'] == USER_STATUS['IN_HOUSE'] else USER_STATUS['IN_HOUSE']
+                    )
 
-                        # Verificar el estado del último log
-                        new_status = USER_STATUS['OUT_HOUSE'] if last_log['attributes']['status'] == USER_STATUS['IN_HOUSE'] else USER_STATUS['IN_HOUSE']
-                        json_data = {
-                            "data": {
-                                "house_access_control": access['id'],
-                                "status": new_status
-                            }
+                    json_data = {
+                        "data": {
+                            "house_access_control": access['id'],
+                            "status": new_status
                         }
+                    }
 
-                        # Determinar si el usuario está entrando o saliendo de la casa
-                        action = "Saliendo de la casa" if new_status == USER_STATUS['OUT_HOUSE'] else "Entrando a la casa"
-                        print(action)
-                        api_request('POST', log_api_url, json_data)
-                        return new_status
-        return None
+                    # Determinar si el usuario está entrando o saliendo de la casa
+                    action = "Saliendo de la casa" if new_status == USER_STATUS['OUT_HOUSE'] else "Entrando a la casa"
+                    print(action)
+                    servo_control()
+
+                    api_request('POST', log_api_url, json_data)
+
+                    if new_status == USER_STATUS['IN_HOUSE']:
+                        users_in_house += 1
+                    elif new_status == USER_STATUS['OUT_HOUSE']:
+                        users_in_house -= 1
+
+                    # Retornar True si hay alguien en casa o el nuevo estado es IN_HOUSE
+                    sendNotification(f"Un usuario esta {action}.", house['id'], STATE_STATUS['SUCCESS'])
+                    return True if users_in_house > 0 else False
+
+        # Si ninguna tarjeta coincide
+        print("Tarjeta no registrada o no activada", house['id'])
+        sendNotification("Una tarjeta no registrada intento acceder a la casa.", house['id'], STATE_STATUS['WARNING'])
+        return None  # Cambié el retorno aquí a False para mayor claridad
 
     except Exception as e:
         print("Error en la solicitud a la API access control:", str(e))
-        return None
+        return None  # También retorné False en caso de error
 
 def house_device_control_status(data, access_status):
     try:
-        for house in data:
-            for category in house['attributes']['home_categories']['data']:
-                for device in category['attributes']['home_devices']['data']:
-                    attributes = device['attributes']
+        # Iterar sobre cada casa en los datos, verificando que data no sea None y tenga la estructura esperada
+        for house in data or []:
+            house_attributes = house.get('attributes', {})
+            
+            # Verificar que 'home_categories' está presente en 'house_attributes'
+            home_categories = house_attributes.get('home_categories', {}).get('data', [])
+            for category in home_categories:
+                category_attributes = category.get('attributes', {})
+                
+                # Verificar que 'home_devices' está presente en 'category_attributes'
+                home_devices = category_attributes.get('home_devices', {}).get('data', [])
+                for device in home_devices:
+                    attributes = device.get('attributes', {})
 
-                    # Verificar código y estado de activación
-                    if attributes['status'] == DEVICE_STATUS['ACTIVATED'] and attributes['code'] in RELAY_PIN_CODE:
-                        relay_status = 0 if access_status == USER_STATUS['IN_HOUSE'] else 1
+                    # Verificar el estado y el código del dispositivo
+                    if attributes.get('status') == DEVICE_STATUS['ACTIVATED'] and attributes.get('code') in RELAY_PIN_CODE:
+                        # Determinar el estado del relay basado en access_status
+                        relay_status = 0 if access_status else 1
+                        # Controlar el relay del dispositivo
                         control_relay(attributes['code'], relay_status)
                         print(f"Dispositivo {attributes['code']} controlado con estado {'OFF' if relay_status else 'ON'}.")
-                        return True
-                
-        return False
+                        time.sleep(1)
+
+                    else:
+                        print(f"Dispositivo {attributes.get('code')} no controlado.")
+
+
+        return True
 
     except Exception as e:
         print("Error en la solicitud a la API device control:", str(e))
-        return False
+        return False 
+
 
 def control_relay(device_id, status):
     relay_pin = RELAY_PIN_CODE[device_id]
     relay = machine.Pin(relay_pin, machine.Pin.OUT)
     relay.value(status)
+
+def sendNotification(message, id, feedback):
+    URL_FETCH_NOTIFICATION = f"{BASE_API_URL}{URL_NOTIFICATION}"
+    data = {
+        "data": {
+            "description": message,
+            "house" : id,
+            "feedback" : feedback
+        }
+    }
+    response = api_request('POST', URL_FETCH_NOTIFICATION, data)
+    if response is not None:
+        return True
+    else:
+        return
 
 connectWifi(SSID, PASSWORD)
 
@@ -184,6 +255,7 @@ def handle_access(rfidInfo):
         access_data = api_request('GET', URL_FETCH_ACCESS)
 
         if access_data is None:
+            sendNotification("No se obtuvieron datos de acceso.", access_data['data'][0]['id'], STATE_STATUS['ERROR'])
             print("No se obtuvieron datos de acceso")
             return
 
@@ -198,15 +270,15 @@ def handle_access(rfidInfo):
         device_data = api_request('GET', URL_FETCH_DEVICE)
 
         if device_data is None:
+            sendNotification("No se obtuvieron datos de dispositivos.", extract_access[0]['id'], STATE_STATUS['ERROR'])
             print("No se obtuvieron datos de dispositivos")
             return
         
         extract_device = extract_info(device_data, 'data')
-
+        
         if not house_device_control_status(extract_device, access_status):
             print("No se controló ningún dispositivo")
-            return
-            
+        
     except Exception as e:
         print("Error en la solicitud a la API:", str(e))
 
